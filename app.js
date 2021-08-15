59f61ee5ea07b59de1011c3f5e0be07a7e58de14
usmansafk <usman230@hotmail.co.uk>
2021-08-15 18:16:25 +0100

    Assign Variables

--- app.js
+++ app.js
@@ -13,13 +13,12 @@
 // console.log("Usman Sajid");
 
 let name = "usman";
+let address, postcode, country;
+
+address = "212 Bixie Lane";
+postcode = "G99 ZXC";
+country = "Scotland";
+
+name = "namsu";
+console.log(address, postcode, country);
 console.log(name);
-//some code here
-console.log(name);
-//some code here
-console.log(name);
-//some code here
-console.log(name);
-//some code here
-console.log(name);
-//some code here
