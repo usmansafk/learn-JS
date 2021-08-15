7e1bbaf707d1fde684bb05bbc4a9a809632df010
usmansafk <usman230@hotmail.co.uk>
2021-08-15 17:42:51 +0100

    helper methods

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 document.write("hello world");
-// alert("hello world");
+alert("hello world");
 console.log("hello world");
+
+//These three are what you call helper methods and are extremely useful
