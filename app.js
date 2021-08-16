b94e6ef4f6a70370a1a17a784d6a874fff6c1588
usmansafk <usman230@hotmail.co.uk>
2021-08-16 12:01:57 +0100

    Numbers Operators

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-// Numbers
-//Loosely Typed, don't declare the type
+// Numbers -- Additional Operators
+// += -= /= *= -- %
+// % returns the remainder after integer division
 
-const number = 55;
-let pants = 3.14;
-pants = "are required for this job";
-const number2 = 2.345;
-const stringNumber3 = "2.345"; // this is a string
-
+let number = 40;
+number += 5;
+number -= 10;
+number++;
+number--;
 console.log(number);
-console.log(pants);
-console.log(number2);
-console.log(stringNumber3); // this is a string - not a number
 
-//NOTE!!
-//IN THE CONSOLE, NUMBERS ARE BLUE.
-//IF A NUMBER IS BLACK THIS MEANS IT IS A STRING!
+const slices = 10;
+const children = 4;
+// const amount = slices / children;
+const amount = slices % children;
 
-const add = number + number2;
-const sub = number - number2;
-const mult = number * number2;
-const div = number / number2;
-
-console.log(add);
-console.log(sub);
-console.log(mult);
-console.log(div);
-
-const test = number + stringNumber3;
-console.log(test); //the number is converted to a string here
-//out put is 552.345
-//aka 55(2.345)
+console.log(amount);
