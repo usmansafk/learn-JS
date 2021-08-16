a0379b8ac597801d2e2063f785754e405fd3c89e
usmansafk <usman230@hotmail.co.uk>
2021-08-16 11:52:34 +0100

    Number

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-// String Concatenation
-// means combining values using +
-// `` back ticks (templates strings) easier option
+// Numbers
+//Loosely Typed, don't declare the type
 
-const fName = "Usman";
-const lName = "Sajid";
-const fullName = fName + " " + lName;
+const number = 55;
+let pants = 3.14;
+pants = "are required for this job";
+const number2 = 2.345;
+const stringNumber3 = "2.345"; // this is a string
 
-console.log("My name is " + fName + " " + lName);
-console.log("My name is " + fullName);
+console.log(number);
+console.log(pants);
+console.log(number2);
+console.log(stringNumber3); // this is a string - not a number
 
-const website = "google";
-const url = "https://www." + website + ".com";
+//NOTE!!
+//IN THE CONSOLE, NUMBERS ARE BLUE.
+//IF A NUMBER IS BLACK THIS MEANS IT IS A STRING!
 
-console.log(url);
+const add = number + number2;
+const sub = number - number2;
+const mult = number * number2;
+const div = number / number2;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+
+const test = number + stringNumber3;
+console.log(test); //the number is converted to a string here
+//out put is 552.345
+//aka 55(2.345)
