dbe265006a6333d32a1fc40c7323a83e3a8b0979
usmansafk <usman230@hotmail.co.uk>
2021-08-16 17:40:16 +0100

    Switch

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-//Logical Operators
-// II - OR
-// && - AND
-// ! - NOT
+//Switch
+//Another way to write if/else statements
 
-const name = "James";
-const age = 23;
+//dice values: 1 - 6;
 
-if (name === "Bob" || age === 23) {
-  console.log("hello there user");
-} else {
-  console.log("wrong values");
+//if approach:
+// const dice = 1;
+// if (dice === 1) {
+//   console.log("you got 1");
+// }
+
+// if (dice === 2) {
+//   console.log("you got 2");
+// }
+
+// if (dice === 3) {
+//   console.log("you got 4");
+// }
+
+// if (dice === 5) {
+//   console.log("you got 5");
+// }
+
+// if (dice === 6) {
+//   console.log("you got 6");
+// }
+
+// if (dice < 1 || dice > 6) {
+//   console.log("you did not roll the dice");
+// }
+
+//switch statement:
+const dice = 1;
+
+switch (dice) {
+  case 1:
+    console.log("you got 1");
+    break;
+  case 2:
+    console.log("you got 2");
+    break;
+  case 3:
+    console.log("you got 3");
+    break;
+  case 4:
+    console.log("you got 4");
+    break;
+  case 5:
+    console.log("you got 5");
+    break;
+  case 6:
+    console.log("you got 6");
+    break;
+  default:
+    console.log("you did not roll the dice");
 }
