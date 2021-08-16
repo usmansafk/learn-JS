8582a4303458800d6df50c5969e10747c7e27ddc
usmansafk <usman230@hotmail.co.uk>
2021-08-16 17:22:25 +0100

    Conditional Operators

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
-//Objects
-// are a collection of properties(keys)
-// each key has a value
-// if the property value has a function, then it is called method
+//Conditional Operators
+// > < >=  <= == === != !==
 
-//Object
-const person = {
-  name: "Usman",
-  age: 24,
-  skin: "brown",
-  hair: "black",
-  isCool: true,
-  education: true,
-  married: true,
-  family: ["Mum", "Dad", "Brother", "Sister"], // array inside an Object
-  greeting: function () {
-    console.log("Hello!");
-  }, // yes- you can store function inside an object!
-  //since ES6 you actually dont even need function keyword for methods inside objects:
-  ES6greeting() {
-    console.log("Hello ES6 style");
-  }
-};
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
 
-//accessing the Object:
-//use dot notation
-console.log(person.name); //output: Usman
-console.log(person.family[2]); //output: Brother
-console.log(person.greeting); //outputs full function
+const value = false;
 
-//to run method within object:
-person.greeting(); //output: Hello!
-person.ES6greeting(); //output: Hello ES6 style
+if (!value) {
+  console.log("The not operator is always the opposite");
+}
 
-//can assign variables from the key/value of an object:
-const myAge = person.age;
-console.log(myAge);
-
-//can modify value of an object:
-person.name = "Usman Sajid";
-console.log(person.name); //outputs: Usman Sajid
-console.log(person); //prints updated name in full object
+if (result) {
+  console.log("first num is greater");
+} else {
+  console.log("second num is greater");
+}
