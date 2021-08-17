c743c3b380f60bbd7e1cc0ad49d6c46f1a1c8119
usmansafk <usman230@hotmail.co.uk>
2021-08-17 12:07:23 +0100

    Variable Lookup

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
-// Global Scope vs Local Scope
+// Variable Lookup
+//{} - code block
 
-// ### Global Scope
-// - any variable outside the code block {} is Global Scoped
-// - **It can be accessed anywhere**
-// - Gotchas: hence, beware of name collisions and modifying by mistake
+const globalNumber = 5;
 
-let name = "bobo";
-name = "peter";
-
-// const name = "john"; // cant do this!
-
-function calc() {
-  //some code here
-  console.log(name);
-  name = "orange"; // can be modified
+function add(num1, num2) {
+  const result = num1 + num2 + globalNumber; //here js will check if globalNumber exists in local scope - since it doesn't it next checks global scope. if it is not there in global scope you will get an error
+  return result;
 }
 
-calc();
-console.log(`my name is ${name} and im cool`); // my name is orange and im cool
-
-// ### Local Scope
-
-// - any variable inside a code block {} is Local Scoped
-// - **It CAN NOT be accessed from outside the code block**
-// - excluding VARs
-
-let wifeyName = "hope";
-
-function sayHi() {
-  const wifeyName = "rija"; //allows same name and doesn't change original
-  const age = 24;
-  //code goes here
-  //everything you do inside the code block
-  return wifeyName;
-}
-
-sayHi();
-console.log(wifeyName); // prints hope not rija
-// console.log(age); // reference error - age is not defined
-
-console.log(`my wifeys name is ${wifeyName} and shes lame`);
+console.log(add(3, 4)); // 12
