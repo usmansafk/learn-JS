186f100c3248180b9511ad4fe45646326cdfad2e
usmansafk <usman230@hotmail.co.uk>
2021-08-17 11:27:14 +0100

    Null and Undefined

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-//Reference vs Value
+//Null and Undefined
+// Both represent "no value"
 
-//**🔑 when assigning primitive data type value to a variable, any changes are made directly to that value, without affecting the original value**
-//**🔑 when assigning non-primitive data type value to a variable, it is done by reference so any changes will affect ALL the references - original reference as well**
+// undefined - "js can not find value for this"
+// variable without value
+// missing function arguments
+// missing object properties
 
-//primitive
-let number = 1;
-let number2 = number;
-number2 = 2;
-console.log(`The first number is ${number}`); //The second number is 1
-console.log(`The second number is ${number2}`); //The second number is 2
+// null - "developer sets the value"
 
-//object
-let person = { name: "bob" };
-let person2 = person;
-person2.name = "susan";
-console.log(`The name of the first person is ${person.name}`); //The name of the second person is susan <-- changed original value
-console.log(`The name of the second person is ${person2.name}`); //The name of the second person is susan
+let number = 20 + null; // 20 + 0;
+console.log(number); // output: 20
+
+let number2 = 20 + undefined; // 20 + 0;
+console.log(number2); // output: NaN
