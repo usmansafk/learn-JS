23aab23fbc8940e0c3820f0246a751de2e031c34
usmansafk <usman230@hotmail.co.uk>
2021-08-17 16:39:59 +0100

    find

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 // Powerful Array Methods:
-// filter --
-// Does return a new array
-// Can manipulate the size of new array
-// returns based on condition, unlike map
+// find --
+// returns single instance - in this case object
+// returns just the first match, if no match then returns undefined
+// great for getting a unique value
 
 const people = [
   {
-    name: "bob",
-    age: 20,
-    position: "dev"
+    name: "usman",
+    age: 24,
+    position: "dev",
+    id: 1
   },
   {
     name: "peter",
     age: 25,
-    position: "designer"
+    position: "designer",
+    id: 2
   },
   {
     name: "susan",
     age: 30,
-    position: "boss"
+    position: "boss",
+    id: 3
   }
 ];
 
-const youngPeople = people.filter(function (person) {
-  return person.age <= 25;
+const findById = people.find(function (person) {
+  return person.id === 3;
 });
 
-const developers = people.filter(function (person) {
-  return person.position === "dev";
+const findMe = people.find(function (person) {
+  return person.name === "usman";
 });
 
-console.log(youngPeople);
-console.log(developers);
+console.log(findMe);
+console.log(findById);
+
+//filter is different from find as it returns a new array and for you to access an item, you must first give the index position:
+const findMe2 = people.filter(function (person) {
+  return person.name === "usman";
+});
+
+console.log(findMe2[0].name);
