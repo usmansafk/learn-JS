92172819bff0836b3d5427a16af44c08210ef831
usmansafk <usman230@hotmail.co.uk>
2021-08-17 16:23:18 +0100

    filter

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//Powerful Array Methods
-//map -
-// most powerful and popular array method, used in frameworks like REACT
-// does returns a new array, unlike forEach
-// does not change size of original array, unlike filter
-// use values from original array when making new one
+// Powerful Array Methods:
+// filter --
+// Does return a new array
+// Can manipulate the size of new array
+// returns based on condition, unlike map
 
 const people = [
   {
@@ -23,32 +22,13 @@ const people = [
   }
 ];
 
-//example 1 -
-const ages = people.map(function (person) {
-  return person.age;
+const youngPeople = people.filter(function (person) {
+  return person.age <= 25;
 });
 
-//example 2 -
-const newPeople = people.map(function (person) {
-  return {
-    firstName: person.name.toUpperCase(),
-    oldAge: person.age + 20 // can include any logic
-  };
+const developers = people.filter(function (person) {
+  return person.position === "dev";
 });
 
-//map is EXTREMELY useful
-//example task 3 -
-//iterate over the peoples array using map
-// extract the name value
-//place it in a h1
-//display it on the screen
-const names = people.map(function (person) {
-  return `<h1>${person.name}</h1>`;
-});
-
-document.body.innerHTML = names.join("");
-
-//logging each to console
-console.log(`Ages: ${ages}`);
-console.log(newPeople);
-console.log(names);
+console.log(youngPeople);
+console.log(developers);
