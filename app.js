d4f55b286cbe15af113513fde6d21922ed8cf4f7
usmansafk <usman230@hotmail.co.uk>
2021-08-17 11:03:13 +0100

    EXERCISE - Calculate Total

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-//EXERCISE  - Full Name
-//Arrays and for loops
+// EXERCISE - Calculate Total
+// Functions, return, if, arrays for loop
 
-//Task - Insert the last name into each item of names array to give the full name
-const siblingsNames = ["usman", "saleha", "sulaiman"];
-const lastName = "sajid";
+//create a function that iterates over an array and returns total
 
-let newArray = [];
+const gas = [20, 40, 100];
+const food = [10, 20, 30, 5];
 
-//for loop
-for (let i = 0; i < siblingsNames.length; i++) {
-  newArray.push(`${siblingsNames[i]} ${lastName}`);
+function calcTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  //insert an if statement, if expenditure is greater than 100, alert!
+  if (total > 100) {
+    console.log(`Whoa! You're spending way too much`);
+    return total;
+  }
+  console.log(`You're spending is good. You've spent less than 100`);
+  return total;
 }
 
-console.log(newArray);
+const gasTotal = calcTotal(gas);
+// console.log(gasTotal);
+const foodTotal = calcTotal(food);
+// console.log(foodTotal);
+const randomTotal = calcTotal([50, 23, 2]);
+// console.log(randomTotal);
+
+// assign each total into an object and log it:
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+  random: randomTotal
+});
