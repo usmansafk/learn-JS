a43a1a1673c9f29e115dc03a8358375881917b4e
usmansafk <usman230@hotmail.co.uk>
2021-08-18 14:49:02 +0100

    Converting elem from NodeList Object to ArrayList Object

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-//getElementByTagName()
-//node-list = array-list like objects
-//must use index for specific items in tag
-//length property but not array methods
+//Converting an element from NodeList Object to Array List Object
+//USE SPREAD OPERATOR!
 
-//elements selection:
-const headings = document.getElementsByTagName("h2");
-const olSection = document.getElementsByTagName("ol");
+//elements selection----
 const fruits = document.getElementsByTagName("li");
 
-//elements modifications:
-headings[0].style.color = "magenta";
-headings[0].style.fontFamily = "Comic Sans MS";
+//elements modifications-----
+
+// fruits.forEach(function (item) {
+//   console.log(item);
+// });
+// THIS WILL NOT WORK- THIS IS BECAUSE IT IS A NODE OBJECT NOT AN ARRAY OBJECT
+//THUS, YOU CAN NOT USE forEach() on node items
+
+//options:
+//1- set up a regular for loop to iterate over the node list
+//2- change node list to array list, using the ES6 way, to use array methods on it:
+const betterFruits = [...fruits]; // via spread operator
+
+betterFruits.forEach(function (item) {
+  //   console.log(item); // this now works!
+});
+
+console.log(fruits);
+console.log(betterFruits);
+//from these two logs, you can clearly see one is an node and one is an array. You can also see the various properties and methods available for each.
