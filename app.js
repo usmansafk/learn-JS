74d2d0d064a33ad1b62f6a43c3dab378f8add3a9
usmansafk <usman230@hotmail.co.uk>
2021-08-18 14:16:50 +0100

    getElementByTagName()

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-//getElementById()
-// Select the element or group of elements, the ones we want
-// Decide the effect we want to apply to the selection
+//getElementByTagName()
+//node-list = array-list like objects
+//must use index for specific items in tag
+//length property but not array methods
 
-// getElementById("element");
+//elements selection:
+const headings = document.getElementsByTagName("h2");
+const olSection = document.getElementsByTagName("ol");
+const fruits = document.getElementsByTagName("li");
 
-//change the colour of the title text using getElementById()
-//method 1 - directly changing it
-document.getElementById("title").style.color = "green";
+//elements modifications:
+headings[5].style.color = "magenta";
+headings[5].style.fontFamily = "Comic Sans MS";
+headings[5].style.fontSize = "70px";
 
-//method 2 (preferred) - using a variable
-const heading = document.getElementById("title");
-heading.style.color = "red";
+headings[6].style.color = "red";
+headings[6].style.fontWeight = "100";
+headings[6].style.fontSize = "50px";
 
-//now, change the bgc and color of the button using method 2
-const clickMeBtn = document.getElementById("btn");
-clickMeBtn.style.backgroundColor = "pink";
-clickMeBtn.style.color = "red";
+olSection[0].style.backgroundColor = "black";
+olSection[0].style.fontSize = "40px";
+
+//use the length property to find out index position:
+console.log(fruits.length);
+
+fruits[13].style.color = "red";
+fruits[14].style.color = "orange";
+fruits[15].style.color = "green";
+fruits[16].style.color = "yellow";
+fruits[17].style.color = "blue";
