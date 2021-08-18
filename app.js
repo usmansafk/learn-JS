ca6602f1d38d0417df357b046e37f3e5f224f6a9
usmansafk <usman230@hotmail.co.uk>
2021-08-18 13:04:34 +0100

    Window AND Document Overview

---
+++ app.js
@@ -0,0 +1,9 @@
+// window object - browser api - the browser giving options to work with the browser
+// document
+// console.dir - allows you to see all the properties and methods available for the node object you have selected
+
+console.dir(document);
+
+// In this case, all the properties and methods on the document objectt that can be accessed
+
+// *The window represents the current tab ~ the document represents the current HTML DOCTYPE page*
