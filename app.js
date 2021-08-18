fd094d09298e2a03df6ba5853a6638e44eb35711
usmansafk <usman230@hotmail.co.uk>
2021-08-18 16:01:20 +0100

    querySelector() and querySelectorAll()

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-//getElementByClassName()
-//also 0 index based
+// querySelector() and querySelectorAll()
+// querySelector('any css') - selects single
+// querySelectorAll('any css') - selects all - can also use forEach()
+// you MUST pass in either . for class or # for id or any CSS selection
 
-const fruitItems = document.getElementsByClassName("fave");
-//this is a node-list object;
-// console.log(fruitItems);
-fruitItems[0].innerHTML = "MANGO";
-fruitItems[0].style.color = "orange";
+//JSYK, you can do this also simply by getElementById()
+const result = document.querySelector("#result");
+result.style.backgroundColor = "silver";
+
+const item = document.querySelector(".special");
+console.log(item);
+//you would think this would print ALL the special classed items, however no!
+//querySelector() only returns the first instance
+//you must use querySelectorAll() if you want to return the whole list
+
+const lastItem = document.querySelector("li:last-child"); // you can access via very specific css like so
+console.log(lastItem);
+
+const specialList = document.querySelectorAll(".special");
+console.log(specialList);
+//from here you can use forEach() even though it is a node-list object
+//this is because it uses the querySelectorAll() method
+
+specialList.forEach(function (item) {
+  console.log(item);
+  item.style.color = "blue";
+  item.style.fontSize = "25px";
+});
+
+//Summary --
+//Therefore, technically you may replace any previous selector so that you do not have the need to change the node-list to array-list
