63087d3928e56e7eb55401ec2b8b3732097d133c
usmansafk <usman230@hotmail.co.uk>
2021-08-18 17:47:58 +0100

    Navigate the DOM - previousSibling and nextSibling

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-//Navigate the DOM - parent
+//Navigate the DOM - previousSibling and nextSibling
+//returns whitespace
 
-//parentElement - same idea as previous childElement
+// Siblings are those which are equal in the DOM structure
+// When you use these properties, the first entry will be a empty text whitespace. Hence you must use the property again to get the correct element:
 
-const heading = document.querySelector("h2");
-console.log(heading.parentElement.parentElement.parentElement.parentElement);
-//as you can see, you can chain the parentElement, until eventually you will reach html element. Beyond that is null.
+const first = document.querySelector(".first"); //notice -- don't forget the . as it references a class
+// console.log(first); // always log if you are ever unsure and to make sure you have selected properly and/or get the correct data set
 
-const parent = heading.parentElement;
-parent.style.color = "red";
-//styling via parent. The whole div
+// let second = first.nextSibling;
+//lets say, you want to see the next sibling to element, use nextSibling property
+// console.log(second);
+//this will originally return text whitespace
+
+//hence:
+const second = first.nextSibling.nextSibling;
+second.style.color = "red"; // changing the colour of list item 2 to red
+console.log(second);
+
+const last = document.querySelector("#last");
+const third = (last.previousSibling.previousSibling.style.color = "blue"); // changing the colour of list item 3 to blue
+console.log(third);
