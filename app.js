463af7d3d3ad9ac8075f3d5fd2c672dbfb7c8633
usmansafk <usman230@hotmail.co.uk>
2021-08-18 14:26:02 +0100

    Created Seperate Page for oon Website for Section3-Dom-Notes

--- app.js
+++ app.js
@@ -9,22 +9,5 @@ const olSection = document.getElementsByTagName("ol");
 const fruits = document.getElementsByTagName("li");
 
 //elements modifications:
-headings[5].style.color = "magenta";
-headings[5].style.fontFamily = "Comic Sans MS";
-headings[5].style.fontSize = "70px";
-
-headings[6].style.color = "red";
-headings[6].style.fontWeight = "100";
-headings[6].style.fontSize = "50px";
-
-olSection[0].style.backgroundColor = "black";
-olSection[0].style.fontSize = "40px";
-
-//use the length property to find out index position:
-console.log(fruits.length);
-
-fruits[13].style.color = "red";
-fruits[14].style.color = "orange";
-fruits[15].style.color = "green";
-fruits[16].style.color = "yellow";
-fruits[17].style.color = "blue";
+headings[0].style.color = "magenta";
+headings[0].style.fontFamily = "Comic Sans MS";
