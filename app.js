6b18b1e47ad690a82354aecc8230ad75a7a666c9
usmansafk <usman230@hotmail.co.uk>
2021-08-18 17:21:50 +0100

    Navigate the DOM - parent

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// Navigate the DOM - children
-//There will be times were you can not directly select an element through the previous selector methods. We will be therefore have to navigate the DOM tree- known as Traverse the DOM tree
+//Navigate the DOM - parent
 
-// childNodes - returns all childNodes including whitespace which is treated as a text node
-// - children - returns actual children
-// - firstChild - returns first child HOWEVER with whitespace
-// - lastChild - returns last child HOWEVER with whitespace
+//parentElement - same idea as previous childElement
 
-// Steps:
+const heading = document.querySelector("h2");
+console.log(heading.parentElement.parentElement.parentElement.parentElement);
+//as you can see, you can chain the parentElement, until eventually you will reach html element. Beyond that is null.
 
-// 1. Select the highest parent element and then navigate the DOM Tree
-// 2. To select the children of the parent you must use the childNodes, firstChild and lastChild or **children**
-// 3. Note: childNodes, firstChild and lastChild returns childNodes including whitespace which is treated as a text node
-
-const result = document.getElementById("result");
-
-console.log(result.childNodes); // this returns everything including text whitespace
-console.log(result.firstChild); // same here
-console.log(result.lastChild); // same here
-
-//better way -- use children property -- return actual chidlren without text whitespace:
-console.log(result.children);
+const parent = heading.parentElement;
+parent.style.color = "red";
+//styling via parent. The whole div
