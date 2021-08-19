5260441cd6fca218ce141d728fbe2184ddca468b
usmansafk <usman230@hotmail.co.uk>
2021-08-19 15:57:01 +0100

    getAttribute() and setAttribute() methods

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-//nodeValue and textContent
-//both do the same thing - allow you to access the text property within the element
+//getAttribute() and setAttribute() methods
 
-const item = document.getElementById("special");
+//li one
+const first = document.querySelector(".first"); //selectors
+const classValue = first.getAttribute("class"); //get attribute prints value of class here
+const idValue = first.getAttribute("id"); //get attribute prints value of id here
+console.log(first); //prints out whole html lime
+console.log(classValue);
+console.log(idValue);
+console.log(first.textContent); //prints whats inside the element
 
-//nodeValue is a bit more tricky
-const value = item.firstChild.nodeValue;
-console.log(value);
+//a tag
+// const link = document.getElementById("link"); //selectors or...
+const link = document.querySelector("#link"); //selectors - can do any selector
+const showLink = link.getAttribute("href"); //get attribute prints value of href here
+console.log(link); //prints out whole html line
+console.log(showLink); //www.google.co.uk
 
-//textContent is easier as it does not have the extra step of looking via child
-const easyValue = item.textContent;
-console.log(easyValue); // preferred/ easier
+//li two
+const last = link.nextElementSibling; //using previously covered property
+last.setAttribute("class", "first"); // first param is type of element, second is the value!!
+last.textContent = "I also have the class of first now"; //UPDATING THE TEXT INSIDE THE 2ND LI ITEM
+console.log(last); //prints out whole html line
+console.log(last.textContent);
+
+const totalLi = document.querySelectorAll(".first"); //we now have 2 .first class elements in node list
+console.log(totalLi);
