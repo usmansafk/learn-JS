ae32d487016acfa8d1d8904fae747afe34b36fd9
usmansafk <usman230@hotmail.co.uk>
2021-08-23 16:22:05 +0100

    Function Expression

--- app.js
+++ app.js
@@ -5,14 +5,24 @@
 //The reason for callback functions as we do not want the function to run right away, but instead when an event occurs
 //you can either pass in an anonymous function OR create a function and reference it
 
-//Click Event:
-const btn = document.querySelector(".btn"); //select button, returns the nodeObject
-const heading = document.querySelector("h1"); //note - sometimes you may have to select the element within the callback function
-//the nodeObject has a method known as addEventListener().
-btn.addEventListener("click", function () {
-  //Note: In the first param(what event?)- There are many other events methods! Secondly, here we are simply creating an anonymous function for the second param
+//Function Expression:
 
-  console.log("Hey punk! You clicked me!");
+const btn = document.querySelector(".btn");
+const heading = document.querySelector("h1");
 
-  heading.classList.add("red");
-});
+//Task
+//Using function expression,
+//add some logic: when button is clicked check if class is there. If yes, then remove class. If not, add the class.
+
+function changeColours() {
+  const hasClass = heading.classList.contains("red");
+
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+  //voila! you have just made a toggle!
+}
+
+btn.addEventListener("click", changeColours); // do NOT invoke it right away with ()
