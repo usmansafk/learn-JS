40598e1a148a77e97fcd58cfd9ae214db96a7679
usmansafk <usman230@hotmail.co.uk>
2021-08-23 16:48:57 +0100

    Key Events

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
-//Mouse-Events
-// click - fires after full action occurs (will be used in most cases)
-//  these are very specific :
-// mousedown - button is pressed
-// mouseup - button is released
-// mouseenter - moved onto an element (when you hover over the element)
-// mouseleave - moved onto out of an element (when you hover off the element)
+//Key Events
+//this is often used with forms, in which you are listening for keys being pressed
+//keypress - when key is pressed
+//keydown - when key is down
+//keyup - when key is released
 
-const heading = document.querySelector("h1");
-const button = document.querySelector(".btn");
+const nameInput = document.getElementById("name");
 
-button.addEventListener("click", function () {
-  console.log("you clicked me");
-}); //FIRES 3RD
-button.addEventListener("mousedown", function () {
-  console.log("down");
-}); //FIRES 1ST
-button.addEventListener("mouseup", function () {
-  console.log("up");
-}); //FIRES 2ND
-
-heading.addEventListener("mouseenter", function () {
-  heading.classList.add("blue");
+nameInput.addEventListener("keypress", function () {
+  console.log("a key was pressed and released"); //RUNS 3RD
 });
-heading.addEventListener("mouseleave", function () {
-  heading.classList.remove("blue");
+nameInput.addEventListener("keydown", function () {
+  console.log("a key was pressed down"); //RUNS 1ST - THIS RUNS AS LONG AS THE KEY IS HELD DOWN
 });
-//voila! you just learned how to create hover effect using js!
+nameInput.addEventListener("keyup", function () {
+  // console.log("a key was released"); //RUNS 2ND
+  // console.dir(nameInput);// can see all the properties available for element
+  console.log(nameInput.value); // THIS IS EXTREMELY USEFUL FOR FUTURE PROJECTS! - gives you access to the entered value by user
+});
