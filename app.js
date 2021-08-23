8554bf4c779f3956f2765d38d87b94e1d59b5ba1
usmansafk <usman230@hotmail.co.uk>
2021-08-23 12:13:20 +0100

    replaceChild("new", "old")

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-//.insertBefore()
+// replaceChild("new", "old")
 
 // (How to add elements directly/ dynamically
 // Steps:
-//createElement("element")
-//createTextNode("text content")
+// createElement("element")
+// createTextNode("text content")
 
 // element.appendChild(childElement) ---> note: they are other methods besides appendChild to place elements, like:
-// .insertBefore("element", "location")
+// replaceChild("new", "old")
 
 const result = document.querySelector("#result");
-
 const first = document.querySelector(".blue");
 
-const bodyDiv = document.createElement("div"); //create empty element:
-const text = document.createTextNode("a simple body div"); //create text node:
-bodyDiv.appendChild(text); // adding the textnode to the body div
-//INSTEAD USE .insertBefore() here:
+//example 1:
+const bodyDiv = document.createElement("div");
+const text = document.createTextNode("a simple body div");
+bodyDiv.appendChild(text);
 document.body.insertBefore(bodyDiv, result);
 
 //example 2:
 const heading = document.createElement("h1");
 const textHeading = document.createTextNode("Dynamic Heading");
 heading.appendChild(textHeading);
-heading.classList.add("red"); //> you can now even add class to the heading as it is on the DOM:
-
-//INSTEAD USE .insertBefore() here:
+heading.classList.add("red");
 result.insertBefore(heading, first);
+
+//example 3 - replaceChild()
+const subHeading = document.createElement("h2");
+const subHeadingText = document.createTextNode(`This is the subheading`);
+subHeading.classList.add("red");
+subHeading.appendChild(subHeadingText);
+// result.insertBefore(subHeading, first); // instead use replaceChild()
+document.body.replaceChild(subHeading, bodyDiv); // here we are replacing the previous bodyDiv with this new subHeading
 
 console.log(result.children);
