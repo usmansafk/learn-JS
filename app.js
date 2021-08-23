3a39c46e843cfa8ab1283a2a13c10e88e7498cb4
usmansafk <usman230@hotmail.co.uk>
2021-08-23 12:38:17 +0100

    remove() and removeChild()

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-// prepend()
-// innerText
+//remove
+//removeChild
 
-//create a h2 in front of the current h1
-const dynamicHeading = document.createElement("h2");
-dynamicHeading.innerText = "I am a dynamic heading. Lol, this is easier! ";
-document.body.prepend(dynamicHeading);
+//Now that we have covered multiple ways how to add elements to the DOM, lets now see how to remove elements from the DOMNow that we have covered multiple ways how to add elements to the DOM, lets now see how to remove elements from the DOM
+// Will also learn how to use selector methods (querySelector/getElementById etc) on node object that we are getting back from our selection
+
+// const result = document.querySelector("#result")
+const result = document.getElementById("result");
+// result.remove(); // removed!
+
+//remove second heading via removeChild()
+const heading = result.querySelector("h1");
+result.removeChild(heading); //removed!
+console.log(heading);
