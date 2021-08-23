da1e67ab684dd356bb799ac6de040222cbc4f6b3
usmansafk <usman230@hotmail.co.uk>
2021-08-23 13:45:04 +0100

    innerHTML and textContent

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-//remove
-//removeChild
+// innerHTML and textContent: similarities and differences
+// Easiest approach
+// using these properties is much more efficient than insert than insertTextNode etc
+//Note: there are times where you will use either the innerHTML or textContent, depending on circumstance. If you want to make changes with propper  HTML structure, not just text, then you must use innerHTML -  see Task Example below
+const list = document.getElementById("first");
+const div = document.getElementById("second");
+const item = document.querySelector(".item");
 
-//Now that we have covered multiple ways how to add elements to the DOM, lets now see how to remove elements from the DOMNow that we have covered multiple ways how to add elements to the DOM, lets now see how to remove elements from the DOM
-// Will also learn how to use selector methods (querySelector/getElementById etc) on node object that we are getting back from our selection
+console.log(div.textContent); // can see the text inside the div
+console.log(list.innerHTML); // can see the whole HTML content, not just the text
+console.log(list.textContent); // can see the text inside the list
 
-// const result = document.querySelector("#result")
-const result = document.getElementById("result");
-// result.remove(); // removed!
+//Task - create another ul, and grab all the list items - FASTER way!
+const ul = document.createElement("ul");
+//instead of creating a textNode and then appending the child etc..., simply:
+ul.innerHTML = `<li class="item">list item</li><li>list item</li><li>list item</li>`; // we are simply copying and pasting from the HTML!! this is the approach in real life projects
+//also make sure to use template strings (back ticks ``) as it will allow you to name your classes and ids etc with "" and also allow you to pass in dynamic values, see below
+document.body.appendChild(ul);
 
-//remove second heading via removeChild()
-const heading = result.querySelector("h1");
-result.removeChild(heading); //removed!
-console.log(heading);
+const randomVar =
+  "random value inserted as item using expression and template strings";
+
+const ol = document.createElement("ol");
+ol.innerHTML = `<li class="item">list item</li><li>list item</li><li>${randomVar}</li>`;
+document.body.appendChild(ol);
