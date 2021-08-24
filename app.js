3e01c7b8b9f0e5c35f52d69fffac13d83ba44177
usmansafk <usman230@hotmail.co.uk>
2021-08-24 15:04:50 +0100

    submit event lister and how to get a value of form

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-// Event Bubbling Capturing
-// Allows select dynamic elements that were made in js
-// event propagation - order of the events are fired
-// event Bubbling - clicked element first then bubbles up -- default
-// event capturing - fires at the root and fires until reaches target
+//submit event lister
+//prevent default
+//how to get a value
 
-const container = document.querySelector(".container");
-const list = document.querySelector(".list-items");
+const form = document.getElementById("form");
+const name = document.getElementById("name"); //to grab the values, you can grab the value property of INPUTS
+const password = document.getElementById("password"); //to grab the values, you can grab the value property of INPUTS
 
-function showBubbling(e) {
-  console.log("Current Target:", e.currentTarget);
-  console.log("Target:", e.target);
-  if (e.target.classList.contains("link")) {
-    console.log("you clicked on the link");
-  }
-}
-
-function stopPropagation(e) {
-  console.log("you clicked on list");
-  e.stopPropagation(); // method that prevents bubbling.
-}
-
-// container.addEventListener("click", stopPropagation);
-// container.addEventListener("click", showBubbling);
-// list.addEventListener("click", showBubbling);
-// document.addEventListener("click", showBubbling);
-// window.addEventListener("click", showBubbling);
-
-container.addEventListener("click", showBubbling, { current: true }); // the opposite of default bubbling behaviour - capturing: start from root and go down to actual element
-list.addEventListener("click", showBubbling, { current: true });
-document.addEventListener("click", showBubbling, { current: true });
-window.addEventListener("click", showBubbling, { current: true });
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // prevents refresh
+  console.log("Form Submitted");
+  console.log("Username:", name.value);
+  console.log("Password:", password.value);
+});
