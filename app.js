7c71e62c616816c023276a6fded19402ed4bcef5
usmansafk <usman230@hotmail.co.uk>
2021-08-24 12:44:50 +0100

    Event Object Argument. e.type, e..currentTarget, e.preventDefault()

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-//Key Events
-//this is often used with forms, in which you are listening for keys being pressed
-//keypress - when key is pressed
-//keydown - when key is down
-//keyup - when key is released
+//Event Object Argument, e evt
+// provides info about triggered event
+// it is a GIANT object with many properties and methods
+// event.type
+// event.currentTarget
+// this keyword
+// preventDefault() - prevents default behaviour
 
-const nameInput = document.getElementById("name");
+const heading = document.getElementById("head");
+const btn = document.querySelector(".btn");
+const link = document.getElementById("link");
 
-nameInput.addEventListener("keypress", function () {
-  console.log("a key was pressed and released"); //RUNS 3RD
+// heading.addEventListener("click", function () {
+//   heading.classList.add("blue"); // since you spelt heading wrong with an s, it wont work!
+// });
+
+//Hence, you can use event object by using an e paramter and it will always select the element that has then event lister attached to it:
+heading.addEventListener("click", function (event) {
+  event.currentTarget.classList.add("blue");
+  console.log(event); // can see all the methods and properties in event object
+  console.log(event.currentTarget); //this is a useful property - shows you the current element that was clicked on and points to the property that has the addEventListener attached to it
+  console.log(this); //Note: this keyword is tricky! will output same as event objects .currentTarget but will NOT work if it is an arrow functions
 });
-nameInput.addEventListener("keydown", function () {
-  console.log("a key was pressed down"); //RUNS 1ST - THIS RUNS AS LONG AS THE KEY IS HELD DOWN
+
+//event.currentTarget and event.type
+btn.addEventListener("click", function (e) {
+  console.log(e); // can see all the methods and properties in event object
+  console.log(e.currentTarget); //this is a useful property - shows you the current element that was clicked on and points to the property that has the addEventListener attached to it
+  console.log(e.type); //shows you what type of event it is - in this case: click
 });
-nameInput.addEventListener("keyup", function () {
-  // console.log("a key was released"); //RUNS 2ND
-  // console.dir(nameInput);// can see all the properties available for element
-  console.log(nameInput.value); // THIS IS EXTREMELY USEFUL FOR FUTURE PROJECTS! - gives you access to the entered value by user
-});
+
+//e.preventDefault() method
+function someFunc(e) {
+  e.preventDefault(); //prevents the default behaviour - VERY USEFUL TO PREVENT PAGE REFRESH ! useful for forms etc.
+}
+link.addEventListener("click", someFunc);
