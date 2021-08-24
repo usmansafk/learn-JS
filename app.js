db5bf73dca7f75ec886a7f06a5aad39a3418de48
usmansafk <usman230@hotmail.co.uk>
2021-08-24 14:17:05 +0100

    Event Bubbling Capturing

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-// CurrentTarget vs Target
-// currentTarget - always refers to the element to which the event handler has been attached to
-// target - identifies the element on which the event occurred
+// Event Bubbling Capturing
+// Allows select dynamic elements that were made in js
+// event propagation - order of the events are fired
+// event Bubbling - clicked element first then bubbles up -- default
+// event capturing - fires at the root and fires until reaches target
 
-const btns = document.querySelectorAll(".btn");
+const container = document.querySelector(".container");
+const list = document.querySelector(".list-items");
 
-btns.forEach(function (btn) {
-  // forEach is used to select EACH button with the given class
-  btn.addEventListener("click", function (e) {
-    console.log(`currentTarget`, e.currentTarget); //currentTarget- whole element which eventlistenr is attached with
-    e.currentTarget.style.color = "black";
-    console.log("-------------------------------------");
-    console.log(`target`, e.target); //target - specific element which was clicked on
-    event.target.style.color = "green";
-  });
-});
+function showBubbling(e) {
+  console.log("Current Target:", e.currentTarget);
+  console.log("Target:", e.target);
+  if (e.target.classList.contains("link")) {
+    console.log("you clicked on the link");
+  }
+}
+
+function stopPropagation(e) {
+  console.log("you clicked on list");
+  e.stopPropagation(); // method that prevents bubbling.
+}
+
+// container.addEventListener("click", stopPropagation);
+// container.addEventListener("click", showBubbling);
+// list.addEventListener("click", showBubbling);
+// document.addEventListener("click", showBubbling);
+// window.addEventListener("click", showBubbling);
+
+container.addEventListener("click", showBubbling, { current: true }); // the opposite of default bubbling behaviour - capturing: start from root and go down to actual element
+list.addEventListener("click", showBubbling, { current: true });
+document.addEventListener("click", showBubbling, { current: true });
+window.addEventListener("click", showBubbling, { current: true });
